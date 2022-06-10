82f3163ff57e1365e8e2c31e7da65952848cba12
Jake Howard <git@theorangeone.net>
2022-06-10 15:54:31 +0100

    Derive body class for pages

--- website/common/models.py
+++ website/common/models.py
@@ -6,3 +6,7 @@ from wagtail.models import Page
 class BasePage(Page):
     class Meta:
         abstract = True
+
+    @property
+    def body_class(self):
+        return "page-" + self._meta.db_table.replace("_", "-")
