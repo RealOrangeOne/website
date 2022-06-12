a1aab90eb9f5e32896ecd7ea836c85c3215121a6
Jake Howard <git@theorangeone.net>
2022-06-12 21:08:11 +0100

    Add basic homepage layout

--- website/home/models.py
+++ website/home/models.py
@@ -1,5 +1,10 @@
+from django.db import models
+from wagtail.admin.panels import FieldPanel
+
 from website.common.models import BasePage
 
 
 class HomePage(BasePage):
-    pass
+    heading = models.CharField(max_length=128, blank=True)
+
+    content_panels = BasePage.content_panels + [FieldPanel("heading")]
