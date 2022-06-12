d4c7b34a15b42216cee28d9123e49c2dff0096e0
Jake Howard <git@theorangeone.net>
2022-06-12 21:08:37 +0100

    Use corect event when configuring nav dropdown

--- static/src/js/base.js
+++ static/src/js/base.js
@@ -1,9 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("load", () => {
   const navbarBurger = document.getElementById("navbar-burger");
   const navbar = document.getElementById("navbar");
 
   navbarBurger.addEventListener("click", () => {
-    console.log("click");
     navbarBurger.classList.toggle("is-active");
     navbar.classList.toggle("is-active");
   });
