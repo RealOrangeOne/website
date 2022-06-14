8e7be79b736363c8ad4909534809c16d9bff91ed
Jake Howard <git@theorangeone.net>
2022-06-14 20:57:50 +0100

    Only allow 1 homepage

--- website/home/models.py
+++ website/home/models.py
@@ -5,6 +5,8 @@ from website.common.models import BasePage
 
 
 class HomePage(BasePage):
+    max_count = 1
+
     heading = models.CharField(max_length=128, blank=True)
 
     content_panels = BasePage.content_panels + [FieldPanel("heading")]
