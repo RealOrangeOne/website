88b6f4b61be7f0145e7020969fa8cc33eaae2b3e
Jake Howard <git@theorangeone.net>
2022-07-04 19:00:20 +0100

    Get content based on original HTML
    It can't be done post anchoring, else the '#' end up in the ToC

--- website/common/models.py
+++ website/common/models.py
@@ -75,7 +75,7 @@ class BaseContentMixin(models.Model):
 
     @cached_property
     def content_html(self) -> str:
-        return get_content_html(self.body_html)
+        return get_content_html(str(self.body))
 
     @cached_property
     def plain_text(self) -> str:
