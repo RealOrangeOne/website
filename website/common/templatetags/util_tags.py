85afe6c41bf09b4bee650fb8c664e6c8f7ff512d
Jake Howard <git@theorangeone.net>
2022-07-16 16:04:06 +0100

    Add alt tags to image blocks

--- website/common/templatetags/util_tags.py
+++ website/common/templatetags/util_tags.py
@@ -1,5 +1,9 @@
 from django.template import Library
+from django.utils.encoding import force_str
 from wagtail.models import Page
+from wagtail.rich_text import RichText
+
+from website.common import utils
 
 register = Library()
 
@@ -12,3 +16,8 @@ def do_range(stop: int) -> range:
 @register.simple_tag(takes_context=True)
 def pagefullurl(context: dict, page: Page) -> str:
     return page.get_full_url(context["request"])
+
+
+@register.filter()
+def extract_text(html: str | RichText) -> str:
+    return utils.extract_text(force_str(html))
