e7617f24de131178d2cfce1090bc67a9fd43a39b
Jake Howard <git@theorangeone.net>
2022-07-25 20:02:54 +0100

    Populate latest blog post on homepage

--- website/home/models.py
+++ website/home/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.http.request import HttpRequest
 from wagtail.admin.panels import FieldPanel
 from wagtail.images import get_image_model_string
 
+from website.blog.models import BlogPostPage
 from website.common.models import BasePage
 
 
@@ -17,3 +19,12 @@ class HomePage(BasePage):
         FieldPanel("heading"),
         FieldPanel("image"),
     ]
+
+    def get_context(self, request: HttpRequest) -> dict:
+        context = super().get_context(request)
+        context["latest_blog_post"] = (
+            BlogPostPage.objects.live()  # type:ignore[attr-defined]
+            .defer_streamfields()
+            .latest("date")
+        )
+        return context
