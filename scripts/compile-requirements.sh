87f549dfeca240a85a50bccf06920dcfabd8e878
Jake Howard <git@theorangeone.net>
2022-07-29 15:48:16 +0100

    Use backtracking resolver for pip-tools
    It's newer, faster and better

--- scripts/compile-requirements.sh
+++ scripts/compile-requirements.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 cd $(dirname $0)/..
 
-COMMON_ARGS="--quiet --annotation-style=line"
+COMMON_ARGS="--quiet --annotation-style=line --resolver=backtracking"
+
+set -x
 
 pip-compile $COMMON_ARGS -o requirements/base.txt requirements/base.in
 pip-compile $COMMON_ARGS -o requirements/dev.txt requirements/dev.in
