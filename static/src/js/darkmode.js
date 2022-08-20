727ad725d15ec2ab30cef5e7d793dd8a7614a7c5
Jake Howard <git@theorangeone.net>
2022-08-20 15:31:14 +0100

    Add dark mode toggle

--- static/src/js/darkmode.js
+++ static/src/js/darkmode.js
@@ -1,9 +1,13 @@
 const DARK_MODE_CLASS = "dark-mode";
+const STORAGE_KEY = "dark-mode";
+
 const body = document.getElementsByTagName("body")[0];
+const darkModeToggle = document.getElementById("dark-mode-toggle");
 
 const matchesDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
 
 function handleDarkMode(darkMode) {
+  window.localStorage.setItem(STORAGE_KEY, darkMode.toString());
   if (darkMode) {
     body.classList.add(DARK_MODE_CLASS);
   } else {
@@ -11,8 +15,19 @@ function handleDarkMode(darkMode) {
   }
 }
 
-matchesDarkMode.addEventListener("change", (event) =>
-  handleDarkMode(event.matches)
-);
+if (window.localStorage.getItem(STORAGE_KEY) === null) {
+  window.localStorage.setItem(STORAGE_KEY, matchesDarkMode.matches);
+  handleDarkMode(matchesDarkMode.matches);
+} else {
+  handleDarkMode(window.localStorage.getItem(STORAGE_KEY) === "true");
+}
 
-handleDarkMode(matchesDarkMode.matches);
+window.addEventListener("load", () => {
+  matchesDarkMode.addEventListener("change", (event) =>
+    handleDarkMode(event.matches)
+  );
+
+  darkModeToggle.addEventListener("click", () => {
+    handleDarkMode(window.localStorage.getItem(STORAGE_KEY) !== "true");
+  });
+});
