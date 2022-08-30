da89510eee24fbf9a994143bd83a8122fae9ec50
Jake Howard <git@theorangeone.net>
2022-08-30 21:38:12 +0100

    Restart workers after a given number of requests

--- etc/gunicorn.conf.py
+++ etc/gunicorn.conf.py
@@ -5,6 +5,8 @@ accesslog = "-"
 disable_redirect_access_to_syslog = True
 preload_app = True
 bind = "0.0.0.0"
+max_requests = 1200
+max_requests_jitter = 100
 
 # Replace gunicorn's 'Server' HTTP header
 gunicorn.SERVER_SOFTWARE = gunicorn.SERVER = "Wouldn't you like to know"
