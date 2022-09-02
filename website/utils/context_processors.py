8691d11a15c8fac71aac1f0c10f7e3527b734e06
Jake Howard <git@theorangeone.net>
2022-09-02 17:20:03 +0100

    Only render comments in debug

--- website/utils/context_processors.py
+++ website/utils/context_processors.py
@@ -3,4 +3,4 @@ from django.http.request import HttpRequest
 
 
 def global_vars(request: HttpRequest) -> dict:
-    return {"SEO_INDEX": settings.SEO_INDEX}
+    return {"SEO_INDEX": settings.SEO_INDEX, "DEBUG": settings.DEBUG}
