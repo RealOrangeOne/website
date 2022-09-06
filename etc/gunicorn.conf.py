fb453a327787232c6548fae9f1242e9ee120f26f
Jake Howard <git@theorangeone.net>
2022-09-06 08:36:55 +0100

    Run an additional thread so the GIL isn't sitting completely idle

--- etc/gunicorn.conf.py
+++ etc/gunicorn.conf.py
@@ -8,5 +8,8 @@ bind = "0.0.0.0"
 max_requests = 1200
 max_requests_jitter = 100
 
+# Run an additional thread so the GIL isn't sitting completely idle
+threads = 2
+
 # Replace gunicorn's 'Server' HTTP header
 gunicorn.SERVER_SOFTWARE = gunicorn.SERVER = "Wouldn't you like to know"
