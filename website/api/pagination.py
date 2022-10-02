a2d1aff112639ff58564497a65fca1bfcd6f3960
Jake Howard <git@theorangeone.net>
2022-10-02 17:28:20 +0100

    Reduce API page size

--- website/api/pagination.py
+++ website/api/pagination.py
@@ -2,5 +2,5 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPageNumberPagination(PageNumberPagination):
-    page_size = 20
-    max_page_size = 50
+    page_size = 10
+    max_page_size = 30
