fa85c8ba80a92e2d0a9210865dcb9b5655ae9b5a
Jake Howard <git@theorangeone.net>
2022-10-04 20:14:26 +0100

    Cache fragments for longer, and clear when editing page

---
+++ website/common/wagtail_hooks.py
@@ -0,0 +1,23 @@
+from django.core.cache import cache
+from django.core.cache.utils import make_template_fragment_key
+from django.http.request import HttpRequest
+from wagtail import hooks
+from wagtail.models import Page
+
+from website.common.models import BasePage
+
+FRAGMENT_CACHES = {"summary", "content-details"}
+
+
+@hooks.register("after_edit_page")
+def clear_fragment_cache(request: HttpRequest, page: Page) -> None:
+    if not isinstance(page, BasePage):
+        return
+
+    cache.delete_many(
+        [
+            # Empty string is for the empty value of `request.is_preview`
+            make_template_fragment_key(cache_name, [page.id, False])
+            for cache_name in FRAGMENT_CACHES
+        ]
+    )
--- website/utils/context_processors.py
+++ website/utils/context_processors.py
@@ -3,4 +3,11 @@ from django.http.request import HttpRequest
 
 
 def global_vars(request: HttpRequest) -> dict:
-    return {"SEO_INDEX": settings.SEO_INDEX, "DEBUG": settings.DEBUG}
+    # noop caching in preview
+    fragment_cache_ttl = 0 if getattr(request, "is_preview", False) else 3600
+    return {
+        "SEO_INDEX": settings.SEO_INDEX,
+        "DEBUG": settings.DEBUG,
+        "FRAGMENT_CACHE_TTL": fragment_cache_ttl,
+        "FRAGMENT_CACHE_TTL_JITTER": fragment_cache_ttl * 0.1,
+    }
