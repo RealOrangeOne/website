214f10002c0526399fd3007c2d3788ead53d1e5d
Jake Howard <git@theorangeone.net>
2022-10-21 13:20:22 +0100

    Cache latest blog post

--- website/home/models.py
+++ website/home/models.py
@@ -1,5 +1,8 @@
+from typing import Optional, Tuple
+
 from django.db import models
 from django.http.request import HttpRequest
+from django_cache_decorator import django_cache_decorator
 from wagtail.admin.panels import FieldPanel
 from wagtail.images import get_image_model_string
 from wagtail.images.models import Image
@@ -7,6 +10,20 @@ from wagtailmetadata.models import WagtailImageMetadataMixin
 
 from website.common.models import BasePage
 from website.contrib.singleton_page.utils import SingletonPageCache
+
+
+@django_cache_decorator(time=21600)
+def get_latest_blog_post() -> Optional[Tuple[str, str]]:
+    from website.blog.models import BlogPostPage
+
+    try:
+        latest_blog_post = (
+            BlogPostPage.objects.live().public().defer_streamfields().latest("date")
+        )
+    except BlogPostPage.DoesNotExist:
+        return None
+
+    return latest_blog_post.title, latest_blog_post.get_url()
 
 
 class HomePage(BasePage, WagtailImageMetadataMixin):
@@ -38,16 +55,9 @@ class HomePage(BasePage, WagtailImageMetadataMixin):
         return ""
 
     def get_context(self, request: HttpRequest) -> dict:
-        from website.blog.models import BlogPostPage
         from website.search.models import SearchPage
 
         context = super().get_context(request)
-        context["latest_blog_post"] = (
-            BlogPostPage.objects.live()
-            .public()
-            .defer_streamfields()
-            .order_by("-date")
-            .first()
-        )
+        context["latest_blog_post"] = get_latest_blog_post()
         context["search_page_url"] = SingletonPageCache.get_url(SearchPage, request)
         return context
