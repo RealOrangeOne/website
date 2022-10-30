1023c821abe8a1e78c1b5d97888f731609e48d20
Jake Howard <git@theorangeone.net>
2022-10-30 12:14:16 +0000

    Allow gunicorn to use `X-Forwarded-*` header

--- etc/gunicorn.conf.py
+++ etc/gunicorn.conf.py
@@ -1,3 +1,5 @@
+import os
+
 import gunicorn
 
 wsgi_app = "website.wsgi:application"
@@ -13,3 +15,7 @@ threads = 2
 
 # Replace gunicorn's 'Server' HTTP header
 gunicorn.SERVER_SOFTWARE = gunicorn.SERVER = "Wouldn't you like to know"
+
+# Allow `X-Forwarded-*` headers from specific IPs
+if "FORWARDED_ALLOW_IPS" in os.environ:
+    forwarded_allow_ips = os.environ["FORWARDED_ALLOW_IPS"]
