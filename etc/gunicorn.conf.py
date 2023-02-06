378c0f9f44666690ddad606b48a306ad1f1b15b7
Jake Howard <git@theorangeone.net>
2023-02-06 20:03:56 +0000

    Allow all IPs to send forwarded headers

--- etc/gunicorn.conf.py
+++ etc/gunicorn.conf.py
@@ -1,5 +1,3 @@
-import socket
-
 import gunicorn
 
 wsgi_app = "website.wsgi:application"
@@ -9,16 +7,10 @@ preload_app = True
 bind = "0.0.0.0"
 max_requests = 1200
 max_requests_jitter = 100
+forwarded_allow_ips = "*"
 
 # Run an additional thread so the GIL isn't sitting completely idle
 threads = 2
 
 # Replace gunicorn's 'Server' HTTP header
 gunicorn.SERVER_SOFTWARE = gunicorn.SERVER = "Wouldn't you like to know"
-
-# HACK: Allow traefik to send forwarded IPs
-# CIDR isn't supported here sadly
-try:
-    forwarded_allow_ips = socket.gethostbyname("traefik")
-except socket.gaierror:
-    pass
