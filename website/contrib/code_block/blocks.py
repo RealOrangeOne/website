d85065d914a2e207663b0e07afbe2583b81c8485
Jake Howard <git@theorangeone.net>
2023-04-21 21:22:59 +0100

    Colour language tag using GitHub's linguist colours

--- website/contrib/code_block/blocks.py
+++ website/contrib/code_block/blocks.py
@@ -1,12 +1,16 @@
 from typing import Iterator
 
+from django.utils.functional import cached_property
 from django.utils.safestring import mark_safe
 from pygments import highlight
 from pygments.formatters.html import HtmlFormatter
 from pygments.lexers import find_lexer_class, get_all_lexers
 from pygments.lexers.python import PythonConsoleLexer, PythonLexer
+from pygments.lexers.shell import BashLexer, BashSessionLexer
 from pygments.lexers.special import TextLexer
 from wagtail.blocks import CharBlock, ChoiceBlock, StructBlock, StructValue, TextBlock
+
+from .utils import get_linguist_colours
 
 
 def get_language_choices() -> Iterator[tuple[str, str]]:
@@ -16,6 +20,8 @@ def get_language_choices() -> Iterator[tuple[str, str]]:
 
 class CodeStructValue(StructValue):
     LANGUAGE_DISPLAY_MAPPING = {PythonConsoleLexer.name: PythonLexer.name}
+
+    LINGUIST_MAPPING = {BashLexer.name: "Shell", BashSessionLexer.name: "Shell"}
 
     def code(self) -> str:
         lexer = find_lexer_class(self["language"])()
@@ -38,6 +44,25 @@ class CodeStructValue(StructValue):
         Map ugly language names to something more useful
         """
         return self.LANGUAGE_DISPLAY_MAPPING.get(self["language"], self["language"])
+
+    @cached_property
+    def colour(self) -> str | None:
+        """
+        Expose the colour denoted by GitHub's linguist.
+        """
+        linguist_colours = get_linguist_colours()
+
+        language = self.LINGUIST_MAPPING.get(self["language"], self["language"])
+
+        if exact_match := linguist_colours.get(language.lower()):
+            return exact_match
+
+        if language_display_match := linguist_colours.get(
+            self.LANGUAGE_DISPLAY_MAPPING.get(language, "").lower()
+        ):
+            return language_display_match
+
+        return None
 
 
 class CodeBlock(StructBlock):
--- website/contrib/code_block/utils.py
+++ website/contrib/code_block/utils.py
@@ -1,4 +1,25 @@
 from importlib.metadata import version
 
+import requests
+import yaml
+from django_cache_decorator import django_cache_decorator
+
 PYGMENTS_VERSION = version("pygments")
 PYGMENTS_VERSION_SLUG = PYGMENTS_VERSION.replace(".", "-")
+
+LINGUIST_DATA_URL = "https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml"
+
+
+@django_cache_decorator(time=21600)
+def get_linguist_colours() -> dict[str, str]:
+    response = requests.get(LINGUIST_DATA_URL)
+
+    response.raise_for_status()
+
+    linguist_data = yaml.safe_load(response.text)
+
+    return {
+        language.lower(): l["color"]
+        for language, l in linguist_data.items()
+        if l.get("color")
+    }
