bc9bdce31d16422bb321ad3b12bc2a24efb3fe77
Jake Howard <git@theorangeone.net>
2023-06-26 13:36:16 +0100

    Remove duplication on content page feed

--- website/common/views.py
+++ website/common/views.py
@@ -82,27 +82,15 @@ class AllPagesFeed(Feed):
         return item.last_published_at
 
 
-class ContentPageFeed(Feed):
+class ContentPageFeed(AllPagesFeed):
     def __init__(self, posts: PageQuerySet, link: str, title: str):
         self.posts = posts
         self.link = link
         self.title = title
         super().__init__()
 
-    def __call__(
-        self, request: HttpRequest, *args: list, **kwargs: dict
-    ) -> HttpResponse:
-        self.request = request
-        return super().__call__(request, *args, **kwargs)
-
     def items(self) -> PageQuerySet:
         return self.posts
-
-    def item_title(self, item: BaseContentPage) -> str:
-        return item.title
-
-    def item_link(self, item: BaseContentPage) -> str:
-        return item.get_full_url(request=self.request)
 
     def item_description(self, item: BaseContentPage) -> str:
         return item.summary
