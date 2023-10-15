fc3704a97a8cd710657176238af98d7ba7d999b7
Jake Howard <git@theorangeone.net>
2023-10-15 22:29:40 +0100

    Add a basic scroll indicator

--- static/src/js/content.js
+++ static/src/js/content.js
@@ -1,6 +1,17 @@
 require("lite-youtube-embed");
 const GLightbox = require("glightbox");
 
+const SCROLL_INDICATOR = document.getElementById("scroll-indicator");
+const CONTENT = document.querySelector(".container.content");
+
 window.addEventListener("load", () => {
   GLightbox({});
+});
+
+window.addEventListener("scroll", () => {
+  const winScroll =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  const height = CONTENT.getBoundingClientRect().height;
+  const scrolled = Math.min((winScroll / height) * 100, 100);
+  SCROLL_INDICATOR.style.width = `${scrolled}%`;
 });
