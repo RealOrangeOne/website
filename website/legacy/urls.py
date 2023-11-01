6248a0665af156a75b1f92caac928515e9c18658
Jake Howard <git@theorangeone.net>
2023-11-01 09:20:33 +0000

    Use correct view for tags and categories redirect

--- website/legacy/urls.py
+++ website/legacy/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("posts/index.xml", views.PostsFeedView.as_view()),
     path("index.xml", views.AllPagesFeedView.as_view()),
     path("tags/<slug:slug>/", views.TagView.as_view()),
-    path("tags/", views.TagView.as_view()),
-    path("categories/", views.TagView.as_view()),
+    path("tags/", views.TagsView.as_view()),
+    path("categories/", views.TagsView.as_view()),
 ]
