4c600651b643be24592cd791ad60b346e3ad3998
Jake Howard <git@theorangeone.net>
2024-02-05 19:40:39 +0000

    Actually allow customizing the page size

--- website/api/pagination.py
+++ website/api/pagination.py
@@ -3,4 +3,5 @@ from rest_framework.pagination import PageNumberPagination
 
 class CustomPageNumberPagination(PageNumberPagination):
     page_size = 10
+    page_size_query_param = "page_size"
     max_page_size = 25
