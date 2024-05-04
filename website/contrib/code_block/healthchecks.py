ac2ba1ef171a204ef2e781945561a89a6ec3cc8b
Jake Howard <git@theorangeone.net>
2024-05-04 20:53:45 +0100

    Return when a colours error is found

--- website/contrib/code_block/healthchecks.py
+++ website/contrib/code_block/healthchecks.py
@@ -9,6 +9,7 @@ class GitHubLinguistHealthCheckBackend(BaseHealthCheckBackend):
             colours = _get_linguist_colours()
         except Exception as e:
             self.add_error(str(e))
+            return
 
         if colours is None:
             self.add_error("No colours provided")
