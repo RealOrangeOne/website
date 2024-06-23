872fd4fc824676899df6721af797b3ed694dde22
Jake Howard <git@theorangeone.net>
2024-06-23 21:35:13 +0100

    Minify pygments styles

--- website/contrib/code_block/views.py
+++ website/contrib/code_block/views.py
@@ -1,21 +1,16 @@
+import lightningcss
 from django.http import HttpRequest, HttpResponse
-from django.utils.datastructures import OrderedSet
 from django.views.decorators.cache import cache_control
 from pygments.formatters.html import HtmlFormatter
 
 
 @cache_control(max_age=3600)
 def pygments_styles(request: HttpRequest) -> HttpResponse:
-    default_styles = (
-        HtmlFormatter(style="default")
-        .get_style_defs("html:not(.dark-mode) .highlight")
-        .split("\n")
+    default_styles = HtmlFormatter(style="default").get_style_defs(
+        "html:not(.dark-mode) .highlight"
     )
-    dark_styles = (
-        HtmlFormatter(style="monokai")
-        .get_style_defs("html.dark-mode .highlight")
-        .split("\n")
+    dark_styles = HtmlFormatter(style="monokai").get_style_defs(
+        "html.dark-mode .highlight"
     )
-    return HttpResponse(
-        "".join(OrderedSet(default_styles + dark_styles)), content_type="text/css"
-    )
+    compressed = lightningcss.process_stylesheet(default_styles + dark_styles)
+    return HttpResponse(compressed, content_type="text/css")
